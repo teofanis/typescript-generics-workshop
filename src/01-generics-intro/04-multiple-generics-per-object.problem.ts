e4181d9046a5cc931ca1d3fbce70bfd54b6079bf
Teofanis Papadopulos <teos_97@hotmail.com>
2024-06-11 22:06:19 +0100

    complete 4

--- src/01-generics-intro/04-multiple-generics-per-object.problem.ts
+++ src/01-generics-intro/04-multiple-generics-per-object.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (params: { a: unknown; b: unknown }) => {
+const returnBothOfWhatIPassIn = <T, P>(params: { a: T; b: P }) => {
   return {
     first: params.a,
     second: params.b,
