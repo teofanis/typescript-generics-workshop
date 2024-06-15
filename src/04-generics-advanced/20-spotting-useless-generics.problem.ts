90436c2b3d08ba987048f89a275abcc44cc08020
Teofanis Papadopulos <teos_97@hotmail.com>
2024-06-15 21:34:32 +0100

    completed 20

--- src/04-generics-advanced/20-spotting-useless-generics.problem.ts
+++ src/04-generics-advanced/20-spotting-useless-generics.problem.ts
@@ -1,10 +1,10 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = <T1, T2>(params: {
-  a: T1;
-  b: T2;
-}): [T1, T2] => {
+const returnBothOfWhatIPassIn = <T1 extends {
+  a: unknown,
+  b: unknown
+}>(params: T1): [T1['a'], T1['b']] => {
   return [params.a, params.b];
 };
 
